e98507a68fb0e3c8090c60baa3a50cc9ddba8aa8
Akash-Jaiswal <koolakash.jaiswal@gmail.com>
2022-12-11 12:46:02 +0530

    remove the project

--- index.js
+++
@@ -1,8 +0,0 @@
-const ReactNative = require('react-native') 
-const { NativeModules, DeviceEventEmitter } = ReactNative
-const NtlDocumentCamera = NativeModules.NtlDocumentCamera
- 
-module.exports = NtlDocumentCamera
-
- 
-
